ad8ef653537e4db6f4327309b3443ac45263a59b
Martijn Donath <martijn.donath@gmail.com>
2013-10-08 03:19:32 +0200

    publish gaat nog niet helemaal goed

--- com_kampinfo/admin/views/hitcamps/tmpl/default_body.php
+++ com_kampinfo/admin/views/hitcamps/tmpl/default_body.php
@@ -9,6 +9,10 @@
 		<td>
 			<?php echo JHtml::_('grid.id', $i, $item->id); ?>
 		</td>
+		<td align="center">
+			<?php $canPublish = $this->canDo->get('hitcamp.edit.state'); ?>
+			<?php echo JHtml::_('jgrid.published', $item->published, $i, 'hitcamps.', $canPublish, 'cb');?>
+		</td>
 		<td>
 			<?php if ($this->canDo->get('hitcamp.edit')) { ?>
 				<a href="<?php echo JRoute :: _('index.php?option=com_kampinfo&task=hitcamp.edit&id='.(int)$item->id); ?>">
@@ -18,12 +22,14 @@
 				<?php echo $item->naam; ?>
 			<?php } ?>
 		</td>
-		<td>
-			<?php $canPublish = $this->canDo->get('hitcamp.edit.state'); ?>
-			<?php echo JHtml::_('jgrid.published', $item->published, $i, 'hitcamps.', $canPublish, 'cb', $item->publish_up, $item->publish_down);?>
+		<td align="center">
+			<?php echo JHtml::_('jgrid.published', $item->akkoordHitKamp, $i, 'hitcamps.', false, 'cb');?>
 		</td>
 		<td>
 			<?php echo $item->plaats; ?>
+		</td>
+		<td align="center">
+			<?php echo JHtml::_('jgrid.published', $item->akkoordHitPlaats, $i, 'hitcamps.', false, 'cb');?>
 		</td>
 		<td>
 			<?php echo $item->jaar; ?>
--- com_kampinfo/admin/views/hitcamps/tmpl/default_head.php
+++ com_kampinfo/admin/views/hitcamps/tmpl/default_head.php
@@ -4,7 +4,7 @@ defined('_JEXEC') or die('Restricted Access');
 
 $listOrder	= $this->escape($this->state->get('list.ordering'));
 $listDirn	= $this->escape($this->state->get('list.direction'));
-$this->state->set('list.columnCount', '10');
+$this->state->set('list.columnCount', '12');
 ?>
 <tr>
 	<th width="5">
@@ -13,14 +13,20 @@ $this->state->set('list.columnCount', '10');
 	<th width="20">
 		<input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count($this->items); ?>);" />
 	</th>
-	<th>
-		<?php echo JHtml::_('grid.sort', 'COM_KAMPINFO_HITCAMPS_HEADING_NAME', 'naam', $listDirn, $listOrder); ?>
-	</th>
 	<th width="5%">
 		<?php echo JHtml::_('grid.sort', 'JSTATUS', 'published', $listDirn, $listOrder); ?>
 	</th>
 	<th>
+		<?php echo JHtml::_('grid.sort', 'COM_KAMPINFO_HITCAMPS_HEADING_NAME', 'naam', $listDirn, $listOrder); ?>
+	</th>
+	<th>
+		Akkoord<br/>Kamp
+	</th>
+	<th>
 		<?php echo JHtml::_('grid.sort', 'COM_KAMPINFO_HITCAMPS_HEADING_HITSITE', 'plaats', $listDirn, $listOrder); ?>
+	</th>
+	<th>
+		Akkoord<br/>Plaats
 	</th>
 	<th>
 		<?php echo JHtml::_('grid.sort', 'COM_KAMPINFO_HITCAMPS_HEADING_YEAR', 'jaar', $listDirn, $listOrder); ?>
