6f6e204ca7e2698ddbe2cc1f7b669d4524c7c916
Martijn Donath <martijn.donath@gmail.com>
2016-01-02 22:42:38 +0100

    Shanti export HIT 2016, invoerveld bij kamp was verdwenen, tijdzone conversie door database werkt niet op hit.scouting.nl, daarom nu lokaal (php) geconverteerd, Selenium scripts aangepast.

--- com_kampinfo/site/models/shanti.php
+++ com_kampinfo/site/models/shanti.php
@@ -20,37 +20,21 @@ class KampInfoModelShanti extends KampInfoModelParent {
 		
 		$query = $db
 			-> getQuery(true)
-			-> select("p.jaar							AS kampinfo_project_jaar")
-			-> select("c.id								AS kampinfo_camp_id")
-			-> select("p.shantiEvenementId				AS kampinfo_project_evenement_id")
-			-> select("dayofmonth(convert_tz(p.inschrijvingKosteloosAnnulerenDatum, 'GMT', 'Europe/Amsterdam'))	AS frm_cancel_dt1_day")
-			-> select("month(convert_tz(p.inschrijvingKosteloosAnnulerenDatum, 'GMT', 'Europe/Amsterdam'))		AS frm_cancel_dt1_month")
-			-> select("year(convert_tz(p.inschrijvingKosteloosAnnulerenDatum, 'GMT', 'Europe/Amsterdam'))		AS frm_cancel_dt1_year")
-			-> select("dayofmonth(convert_tz(p.inschrijvingGeenRestitutieDatum, 'GMT', 'Europe/Amsterdam'))		AS frm_cancel_dt2_day")
-			-> select("month(convert_tz(p.inschrijvingGeenRestitutieDatum, 'GMT', 'Europe/Amsterdam'))			AS frm_cancel_dt2_month")
-			-> select("year(convert_tz(p.inschrijvingGeenRestitutieDatum, 'GMT', 'Europe/Amsterdam'))			AS frm_cancel_dt2_year")
-			-> select("dayofmonth(convert_tz(p.inningsdatum, 'GMT', 'Europe/Amsterdam'))						AS kampinfo_project_inningsdatum_day")
-			-> select("month(convert_tz(p.inningsdatum, 'GMT', 'Europe/Amsterdam'))								AS kampinfo_project_inningsdatum_month")
-			-> select("year(convert_tz(p.inningsdatum, 'GMT', 'Europe/Amsterdam'))								AS kampinfo_project_inningsdatum_year")
+			-> select("p.jaar									AS kampinfo_project_jaar")
+			-> select("c.id										AS kampinfo_camp_id")
+			-> select("p.shantiEvenementId						AS kampinfo_project_evenement_id")
+			-> select("p.inschrijvingKosteloosAnnulerenDatum	AS frm_cancel_dt1")
+			-> select("p.inschrijvingGeenRestitutieDatum		AS frm_cancel_dt2")
+			-> select("p.inningsdatum							AS kampinfo_project_inningsdatum")
 			-> select("s.naam									AS frm_location_nm")
 			-> select("s.projectcode							AS projectcode")
 			-> select("c.naam									AS frm_nm")
 			-> select("COALESCE(c.isouderkind,0)				AS kampinfo_camp_isouder")
 			-> select("c.deelnamekosten							AS frm_price")
-			-> select("dayofmonth(convert_tz(c.startDatumTijd, 'GMT', 'Europe/Amsterdam'))	 			AS frm_from_dt_day")
-			-> select("month(convert_tz(c.startDatumTijd, 'GMT', 'Europe/Amsterdam'))					AS frm_from_dt_month")
-			-> select("year(convert_tz(c.startDatumTijd, 'GMT', 'Europe/Amsterdam'))					AS frm_from_dt_year")
-			-> select("date_format(convert_tz(c.startDatumTijd, 'GMT', 'Europe/Amsterdam'), '%H:%i')	AS frm_from_time")
-			-> select("dayofmonth(convert_tz(c.eindDatumTijd, 'GMT', 'Europe/Amsterdam'))				AS frm_till_dt_day")
-			-> select("month(convert_tz(c.eindDatumTijd, 'GMT', 'Europe/Amsterdam'))					AS frm_till_dt_month")
-			-> select("year(convert_tz(c.eindDatumTijd, 'GMT', 'Europe/Amsterdam'))						AS frm_till_dt_year")
-			-> select("date_format(convert_tz(c.eindDatumTijd, 'GMT', 'Europe/Amsterdam'), '%H:%i')		AS frm_till_time")
-			-> select("dayofmonth(convert_tz(p.inschrijvingStartdatum, 'GMT', 'Europe/Amsterdam'))		AS frm_book_from_dt_day")
-			-> select("month(convert_tz(p.inschrijvingStartdatum, 'GMT', 'Europe/Amsterdam')) 			AS frm_book_from_dt_month")
-			-> select("year(convert_tz(p.inschrijvingStartdatum, 'GMT', 'Europe/Amsterdam'))			AS frm_book_from_dt_year")
-			-> select("dayofmonth(convert_tz(p.inschrijvingEinddatum, 'GMT', 'Europe/Amsterdam'))		AS frm_book_till_dt_day")
-			-> select("month(convert_tz(p.inschrijvingEinddatum, 'GMT', 'Europe/Amsterdam'))			AS frm_book_till_dt_month")
-			-> select("year(convert_tz(p.inschrijvingEinddatum, 'GMT', 'Europe/Amsterdam'))				AS frm_book_till_dt_year")
+			-> select("c.startDatumTijd							AS frm_from_dt")
+			-> select("c.eindDatumTijd							AS frm_till_dt")
+			-> select("p.inschrijvingStartdatum					AS frm_book_from_dt")
+			-> select("p.inschrijvingEinddatum					AS frm_book_till_dt")
 			-> select("c.minimumLeeftijd						AS frm_min_age")
 			-> select("c.maximumLeeftijd						AS frm_max_age")
 			-> select("c.margeAantalDagenTeJong					AS frm_min_age_margin_days")
@@ -73,6 +57,53 @@ class KampInfoModelShanti extends KampInfoModelParent {
 		$db->setQuery($query);
 		$rows = $db->loadObjectList();
 		
+		$datumVelden = ['frm_cancel_dt1', 'frm_cancel_dt2', 'kampinfo_project_inningsdatum', 'frm_from_dt', 'frm_till_dt', 'frm_book_from_dt', 'frm_book_till_dt'];
+		$tijdVelden = ['frm_from_dt', 'frm_till_dt'];
+		foreach ($rows as $row) {
+			$this->convertDateFromUTC($row, $datumVelden);
+			$this->splitDate($row, $datumVelden);
+			$this->splitTime($row, $tijdVelden);
+			$this->removeDateFields($row, $datumVelden);
+		}
 		return $rows;
 	}
+
+	private function convertDateFromUTC($object, $datumVelden) {
+		$UTC = new DateTimeZone("UTC");
+		$tz = new DateTimeZone("Europe/Amsterdam");
+		foreach ($datumVelden as $veld) {
+			$localDate = DateTime::createFromFormat('Y-m-d H:i:s', $object->$veld, $UTC);
+			$localDate->setTimezone($tz);
+			$object->$veld = $localDate;
+		}
+	}
+	
+	private function splitDate($object, $datumVelden) {
+		foreach ($datumVelden as $veld) {
+			$localDate = $object->$veld;
+			
+			$dag = $veld . '_day';
+			$object->$dag = $localDate->format('d');
+			$maand = $veld . '_month';
+			$object->$maand = $localDate->format('m');
+			$jaar = $veld . '_year';
+			$object->$jaar = $localDate->format('Y');
+		}
+	}
+
+	private function splitTime($object, $datumVelden) {
+		foreach ($datumVelden as $veld) {
+			$localDate = $object->$veld;
+
+			$tijd = substr($veld, 0, -3) . '_time';
+			$object->$tijd = $localDate->format('H:i');
+		}
+	}
+	
+	private function removeDateFields($object, $datumVelden) {
+		foreach ($datumVelden as $veld) {
+			unset($object->$veld);
+		}
+	}
+	
 }
